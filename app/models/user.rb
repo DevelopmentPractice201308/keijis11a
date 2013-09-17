351e231594a52d9823f51287363f63be67f9665f
keijis11a <keiji_s-11a@stu.kbc.ac.jp>
2013-09-17 12:26:44 +0900

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
-<<<<<<< HEAD
   before_save :create_remember_token
 
   before_save { email.downcase! }
-=======
->>>>>>> e6b4e001c629e132f10eb42db5a2be7548af5254
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -17,12 +14,9 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-<<<<<<< HEAD
   private
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
   end
-=======
->>>>>>> e6b4e001c629e132f10eb42db5a2be7548af5254
 end
